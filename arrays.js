5ec97fdda415e862cc8120dc2be986ee3682bd11
Bruno Marim <bruno.br.sp89@gmail.com>
2022-02-19 12:30:15 -0300

    Arrays
    Esta aula apresenta o conceito e a estrutura das variáveis compostas, ou seja, as variáveis que comportam mais de um elemento.
    
    Array/vetor/variável composta
    
    Uma array é formada por vários elementos.
    Cada elemento é formado por um conteúdo/valor
    e por um índice/chave de identificação.
    
    var num = [5,4,9] - exemplo de estrutura de uma var composta.
    num[3] = 6 - Cria um quarto elemento com chave [3] e insere o número 6.
    num.push(7) - Cria mais um elemento e add o número 7 a ele.
    num.length - Dá a quantidade de elementos do vetor.
    num.sort() - Organiza os elementos em ordem crescente.

---
+++ arrays.js
@@ -0,0 +1,8 @@
+var num = [1,2,9,3,7]
+console.log(num)
+console.log(`O número de elementos dessa array é ${num.length}`)
+console.log(`Em ordem crescente eles ficam assim: ${num.sort()}`)
+num.push(8)
+console.log(num)
+num[6]=4
+console.log(num)
